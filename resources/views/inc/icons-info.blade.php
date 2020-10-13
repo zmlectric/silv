5fefb41f81ea2386481985bef5528cb4ed56ddb0
Zamira <zegbers@hotmail.com>
2020-10-13 19:20:02 +0200

    test git

---
+++ resources/views/inc/icons-info.blade.php
@@ -0,0 +1,45 @@
+<div class="container">
+    <div class="row">
+
+    </div>
+
+
+{{--        <div class="col-md-12" style="margin-top: 6em;">--}}
+{{--            <div id="nailart">--}}
+{{--                <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info1.png"></a>--}}
+{{--                <h5 class="info-text" style="text-align: center;">Nailart</h5>--}}
+{{--                <p class="info-text" style="text-align: center;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium assumenda, blanditiis dicta eius eum expedita minima modi pariatur placeat possimus qui ut! Aliquam explicabo facere iusto laboriosam, odio quasi quia.</p>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+
+{{--        <div id="gellak" style="margin-top: 6em;">--}}
+{{--            <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info2.png"></a>--}}
+{{--            <h5 class="info-text" style="text-align: center;">Gellak</h5>--}}
+{{--            <p class="info-text" style="text-align: center; margin-top: 1em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet blanditiis dicta eos fugit, hic laudantium neque, nesciunt numquam pariatur perspiciatis praesentium quis quo reprehenderit similique suscipit tempore voluptatum. Laudantium, sequi.</p>--}}
+{{--        </div>--}}
+
+{{--        <div id="nagelverlenging" style="margin-top: 6em;">--}}
+{{--            <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info3.png"></a>--}}
+{{--            <h5 class="info-text" style="text-align: center;">Nagelverlenging</h5>--}}
+{{--            <p class="info-text" style="text-align: center; margin-top: 1em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos excepturi facere minima, provident quaerat qui quos soluta? Assumenda ipsum laboriosam libero odit pariatur possimus provident quo reiciendis repellendus temporibus.</p>--}}
+{{--        </div>--}}
+
+{{--        <div id="haarkleuring" style="margin-top: 6em;">--}}
+{{--            <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info4.png"></a>--}}
+{{--            <h5 class="info-text" style="text-align: center;">Haarkleuring</h5>--}}
+{{--            <p class="info-text" style="text-align: center; margin-top: 1em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A dolorem et libero magni mollitia nam quidem, quos repellat rerum! Consectetur eius esse iusto mollitia nam nesciunt officia quos sunt voluptatem?</p>--}}
+{{--        </div>--}}
+
+{{--        <div id="knippen" style="margin-top: 6em;">--}}
+{{--            <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info5.png"></a>--}}
+{{--            <h5 class="info-text" style="text-align: center;">lorem</h5>--}}
+{{--            <p class="info-text" style="text-align: center; margin-top: 1em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque exercitationem fuga labore laborum optio quae similique sint sit tempore vitae. Beatae deserunt dolores omnis quisquam similique tempore tenetur totam velit.</p>--}}
+{{--        </div>--}}
+
+{{--        <div id="heren" style="margin-top: 6em;">--}}
+{{--            <a data-scroll><img class="img-icons-info mx-auto d-block" src="/images/icon-info6.png"></a>--}}
+{{--            <h5 class="info-text" style="text-align: center;">lorem</h5>--}}
+{{--            <p class="info-text" style="text-align: center; margin-top: 1em;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias amet, aperiam blanditiis debitis dicta dolore exercitationem fugiat iure magnam necessitatibus non optio pariatur quas, qui quia similique unde ut vero?</p>--}}
+{{--        </div>--}}
+</div>
+
---
+++ resources/views/inc/icons.blade.php
@@ -0,0 +1,25 @@
+<div class="container">
+
+    <div class="row" style="justify-content: center; margin-top: 5em;">
+        <div class="col-md-3">
+            <a href="#nailart"><img class="img-icon mx-auto d-block" src="/images/icon1.png"></a>
+        </div>
+        <div class="col-md-3">
+            <a href="#gellak"><img class="img-icon mx-auto d-block" src="/images/icon2.png"></a>
+        </div>
+        <div class="col-md-3">
+            <a href="#nagelverlenging"><img class="img-icon mx-auto d-block" src="/images/icon3.png"></a>
+        </div>
+    </div>
+    <div class="row" style="justify-content: center;margin-top: 2em;">
+        <div class="col-md-3">
+            <a href="#haarkleuring"><img class="img-icon mx-auto d-block" src="/images/icon4.png"></a>
+        </div>
+        <div class="col-md-3">
+            <a href="#knippen"><img class="img-icon mx-auto d-block" src="/images/icon5.png"></a>
+        </div>
+        <div class="col-md-3">
+            <a href="#heren"><img class="img-icon mx-auto d-block" src="/images/icon6.png"></a>
+        </div>
+    </div>
+</div>
